65b7f1c1f1ae0be300165336d7638f565cad0688
Meowsef <joe@joebutler.dev>
2020-05-11 13:09:40 +1200

    Finish calculate function

--- calculator.js
+++ calculator.js
@@ -50,44 +50,41 @@ function calculate() {
     temp = '';
   }
 
-}
-
-
-/*
-
   // Got the equals sign, perform calculation
-  else if val is equals symbol
-  push temp value to entries array
+  else if (val === "=") {
+    entries.push(temp);
+    let nt = Number(entries[0]);
 
-  declare nt variable which turns the first entry array value and converts it to a number
-  for loop to go through entries array
-    declare nextNum variable which converts entries[i + 1] to a number (skips the math symbol being used)
-    declare symbol variable which gets the math symbol being used;
+    for (j = 0; j < entries.length; j++) {
+      let nextNum = Number([j + 1]);
+      let symbol = entries[j];
 
-    // do the math for that symbol and next entry
-    if the symbol is +, add nextNum to nt
-    else if symbol is -, subtract nextNum from nt
-    else if symbol is *, multiple nextNum by nt
-    else if symbol is /, divide nextNum by nt
+      // do the math for that symbol and next entry
+      if (symbol === '+') {
+        nt += nextNum;
+      } else if (symbol === '-') {
+        nt -= nextNum;
+      } else if (symbol === '*') {
+        nt *= nextNum;
+      } else if (symbol === '/') {
+        nt /= nextNum;
+      }
+      j++;
+    }
 
-    increment i
-  close for loop
+    // Swap the '-' symbol so text input handles it correctly
+    if (nt < 0) {
+      Math.abs(nt) + "-";
+    }
 
-  // Swap the '-' symbol so text input handles it correctly
-  if nt is less than zero
-    convert it to an absolute number and add - string
-
-    display value of nt in the answer input
-    clear the entries array
-    clear temp back to empty string
-
-    display value of nt in the answer input
-
+    display.value(nt);
+    entries = [];
+    temp = '';
+  }
   // Push number
-  else
-    push the temp value to entries
-    push the val value to entries
-    clear temp back to empty string
-  end if statement
-end statement
-*/
+  else {
+    entries.push(temp);
+    entries.push(val);
+    temp = '';
+  }
+}
