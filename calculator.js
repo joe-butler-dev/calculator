ee5a93f9c7d9aff9240b3482b894e0e8c9e33f51
Meowsef <joe@joebutler.dev>
2020-05-11 14:07:40 +1200

    Finish working calculator!

--- calculator.js
+++ calculator.js
@@ -1,6 +1,5 @@
 var entries = [];
 var total = 0;
-
 var temp = '';
 
 var buttons = document.getElementsByTagName('button');
@@ -8,14 +7,15 @@ var display = document.getElementById('answer');
 
 // Add event handlers for each button and run the calculate function when one is pressed
 for (i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener("click", calculate);
+  buttons[i].addEventListener('click', calculate);
 }
 
 function calculate() {
   let val = this.textContent;
+  console.log(val);
 
   // Got a number, add to temp
-  if (isNaN(val) || val === ".") {
+  if (!isNaN(val) || val === '.') {
     temp += val;
     val = temp.substring(0, 10);
     display.value = val;
@@ -25,8 +25,8 @@ function calculate() {
   // then add our current symbol and clear temp
   else if (val === 'AC') {
     entries = [];
-    temp = '';
     total = 0;
+    temp = '';
     display.value = '';
   }
 
@@ -39,27 +39,26 @@ function calculate() {
   // Change multiply symbol to work with eval
   else if (val === 'x') {
     entries.push(temp);
-    entries.push("*");
+    entries.push('*');
     temp = '';
   }
 
   // Change divide symbol to work with eval
   else if (val === '÷') {
     entries.push(temp);
-    entries.push("/");
+    entries.push('/');
     temp = '';
   }
 
   // Got the equals sign, perform calculation
-  else if (val === "=") {
+  else if (val === '=') {
     entries.push(temp);
     let nt = Number(entries[0]);
 
     for (j = 0; j < entries.length; j++) {
-      let nextNum = Number([j + 1]);
+      let nextNum = Number(entries[j + 1]);
       let symbol = entries[j];
 
-      // do the math for that symbol and next entry
       if (symbol === '+') {
         nt += nextNum;
       } else if (symbol === '-') {
@@ -69,18 +68,18 @@ function calculate() {
       } else if (symbol === '/') {
         nt /= nextNum;
       }
-      j++;
     }
 
     // Swap the '-' symbol so text input handles it correctly
     if (nt < 0) {
-      Math.abs(nt) + "-";
+      Math.abs(nt) + '-';
     }
 
-    display.value(nt);
+    display.value = nt;
     entries = [];
     temp = '';
   }
+
   // Push number
   else {
     entries.push(temp);
